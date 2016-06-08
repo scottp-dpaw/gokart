abdb64b286d4fdc46811376e5f0c13f18d8f1851
Adon Metcalfe <adonm@fastmail.fm>
2016-06-08 19:39:29 +0800

    simplify templates and collapse things

--- gokart/static/js/map.js
+++ gokart/static/js/map.js
@@ -1,26 +1,23 @@
 "use strict";
-window.gokart = window.gokart || {};
-
-(function(self) {
-
+window.gokart = (function(self) {
     // default matrix from KMI
     var _matrixSets = {
-        "EPSG:4326" : {
-            "1024":{
-                "name":"gda94",
-                "resolutions" : [ .17578125, .087890625, .0439453125, .02197265625, .010986328125, .0054931640625, .00274658203125, .001373291015625, .0006866455078125, .0003433227539062, .0001716613769531, 858306884766e-16, 429153442383e-16, 214576721191e-16, 107288360596e-16, 53644180298e-16, 26822090149e-16, 13411045074e-16 ],
-                "minLevel" : 0,
-                "maxLevel" : 17,
+        "EPSG:4326": {
+            "1024": {
+                "name": "gda94",
+                "resolutions": [.17578125, .087890625, .0439453125, .02197265625, .010986328125, .0054931640625, .00274658203125, .001373291015625, .0006866455078125, .0003433227539062, .0001716613769531, 858306884766e-16, 429153442383e-16, 214576721191e-16, 107288360596e-16, 53644180298e-16, 26822090149e-16, 13411045074e-16],
+                "minLevel": 0,
+                "maxLevel": 17,
             }
         },
     };
 
     // generate matrix IDs from name and level number
-    $.each(_matrixSets,function(projection,innerMatrixSets){
-        $.each(innerMatrixSets,function(tileSize,matrixSet){
+    $.each(_matrixSets, function(projection, innerMatrixSets) {
+        $.each(innerMatrixSets, function(tileSize, matrixSet) {
             var matrixIds = new Array(matrixSet.maxLevel - matrixSet.minLevel + 1);
             for (var z = matrixSet.minLevel; z <= matrixSet.maxLevel; ++z) {
-                matrixIds[z] =  matrixSet.name + ":" + z;
+                matrixIds[z] = matrixSet.name + ":" + z;
             }
             matrixSet.matrixIds = matrixIds;
         });
@@ -37,11 +34,11 @@ window.gokart = window.gokart || {};
     };
 
     // Convenience loader to create a WMTS layer from a kmi datasource
-    self.create_tile_layer = function(layer) {
+    self.createTileLayer = function(layer) {
         layer = layer || {};
         layer = $.extend({}, self.defaultLayer, layer);
 
-        var matrixSet =  _matrixSets[layer.projection][layer.tileSize];
+        var matrixSet = _matrixSets[layer.projection][layer.tileSize];
         var tileLayer = new ol.layer.Tile({
             opacity: (layer.opacity || 100) / 100,
             source: new ol.source.WMTS({
@@ -53,7 +50,7 @@ window.gokart = window.gokart || {};
                 projection: layer.projection,
                 wrapX: true,
                 tileGrid: new ol.tilegrid.WMTS({
-                    origin: ol.extent.getTopLeft([ -180, -90, 180, 90 ]),
+                    origin: ol.extent.getTopLeft([-180, -90, 180, 90]),
                     resolutions: matrixSet.resolutions,
                     matrixIds: matrixSet.matrixIds,
                     tileSize: layer.tileSize
@@ -63,23 +60,15 @@ window.gokart = window.gokart || {};
         return tileLayer;
     };
 
-    self.init = function() {
+    self.init = function(layers) {
         self.map = new ol.Map({
             logo: false,
             renderer: "canvas",
             target: "map",
-            layers: [
-                self.create_tile_layer({
-                    'name': 'dpaw:mapbox_outdoors'
-                }),
-                self.create_tile_layer({
-                    'name': 'dpaw:telstra_gcm_4g_3g',
-                    'format': 'image/png'
-                }), 
-            ],
+            layers: layers,
             view: new ol.View({
                 projection: "EPSG:4326",
-                center: [ 123.75, -24.966 ],
+                center: [123.75, -24.966],
                 zoom: 5,
                 maxZoom: 21,
                 minZoom: 3
@@ -96,4 +85,5 @@ window.gokart = window.gokart || {};
             ]
         });
     };
-})(window.gokart);
+    return self;
+})(window.gokart || {});
--- gokart/static/js/ui.js
+++ gokart/static/js/ui.js
@@ -1,2 +1,63 @@
-$(document).foundation();
-svg4everybody();
+"use strict";
+window.gokart = (function(self) {
+    $(document).foundation();
+    svg4everybody();
+
+    var ui = self.ui = {};
+
+    ui.sizes = {
+        "tool-size-small": 16,
+        "tool-size-medium": 32,
+        "tool-size-large": 64
+    };
+
+    ui.colours = {
+        "tool-colour-r": "#cc0000",
+        "tool-colour-o": "#f57900",
+        "tool-colour-y": "#edd400",
+        "tool-colour-g": "#73d216",
+        "tool-colour-b": "#3465a4",
+        "tool-colour-v": "#75507b",
+        "tool-colour-br": "#8f5902",
+        "tool-colour-gr": "#555753",
+        "tool-colour-bl": "#000000"
+    };
+
+    // bind menu side-tabs to reveal the side pane
+    var offCanvasLeft = $("#offCanvasLeft")
+    $("#menu-tabs").on("change.zf.tabs", function(ev) {
+        offCanvasLeft.addClass("reveal-for-medium");
+        self.map.updateSize();
+    }).on("click", ".tabs-title a[aria-selected=false]", function(ev) {
+        offCanvasLeft.addClass("reveal-for-medium");
+        $(this).attr("aria-selected", true);
+        self.map.updateSize();
+    }).on("click", ".tabs-title a[aria-selected=true]", function(ev) {
+        offCanvasLeft.removeClass("reveal-for-medium");
+        $(this).attr("aria-selected", false);
+        self.map.updateSize();
+    });
+
+    // rig up groups
+    $("#tool-group-mode").on("click", ".button", function(ev) {
+        $("#tool-group-mode .button").removeClass("selected");
+        $(this).addClass("selected");
+        self.mode = this.id;
+    });
+
+    $("#tool-group-size").on("click", ".button", function(ev) {
+        $("#tool-group-size .button").removeClass("selected");
+        $(this).addClass("selected");
+        self.size = this.id;
+    });
+
+    $("#tool-group-colour").on("click", ".button", function(ev) {
+        $("#tool-group-colour .button").removeClass("selected");
+        $(this).addClass("selected");
+        self.colour = this.id;
+    });
+
+    return self;
+})(window.gokart || {});
+
+
