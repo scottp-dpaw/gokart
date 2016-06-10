aefa24e6c53e3533cd2f6df89ad62cd95ad001c4
Adon Metcalfe <adonm@fastmail.fm>
2016-06-10 18:53:24 +0800

    added todo

--- gokart/__init__.py
+++ gokart/__init__.py
@@ -9,7 +9,6 @@ bottle.TEMPLATE_PATH.append('./gokart/views')
 def index( app_name ):
     return bottle.jinja2_template('app-{}.html'.format( app_name ))
 
-
-
+#TODO: make gdal pdf thingy gdal_translate -of PDF -a_ullr 113.85406494140625 -34.281463623046875 119.12750244140625 -31.257476806640625 -a_srs EPSG:4326 -co DPI=150 /tmp/map.jpg gokart/static/map.pdf
 
 application = bottle.default_app()
