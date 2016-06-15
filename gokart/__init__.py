9600834c336e0de5c6b09730b73ef25744921638
Scott Percival <scott.percival@dpaw.wa.gov.au>
2016-06-15 14:17:07 +0800

    add WMS shim for Himawari-8 tiles

--- gokart/__init__.py
+++ gokart/__init__.py
@@ -1,18 +1,70 @@
-import confy
 import bottle 
-import tempfile 
+import confy
 import os
+import pytz
+import random
 import shutil
 import subprocess
-from datetime import datetime
+import tempfile 
+from datetime import datetime, timedelta
+from six.moves import urllib
+
 
 bottle.TEMPLATE_PATH.append('./gokart/views')
 bottle.debug(True)
 
+# serve up map apps
 @bottle.route('/<app_name>')
 def index( app_name ):
     return bottle.jinja2_template('apps/{}.html'.format( app_name ))
 
+
+# WMS shim for Himawari 8
+# Landgate tile servers, round robin
+LANDGATE_SERVERS = ['http://t{}.srss-ows-6.landgate.wa.gov.au/mapproxy/firewatch/service'.format(x) for x in range(1,6)]
+# timezone used for timestamps
+LANDGATE_TZ = pytz.timezone('Australia/Perth')
+# update period
+LANDGATE_PERIOD = timedelta(minutes=10)
+# approx. lead time before a layer is published 
+HI8_FHS_LEADTIME = timedelta(minutes=3)
+HI8_BAND_LEADTIME = timedelta(minutes=33)
+HI8 = {
+    'band7': ('layer63_{}_HI8_AHI_TKY_b7.tif', HI8_BAND_LEADTIME),
+    'band15': ('layer63_{}_HI8_AHI_TKY_b15.tif', HI8_BAND_LEADTIME),
+    'bandtc': ('layer63_{}_HI8_AHI_TKY_b321.tif', HI8_BAND_LEADTIME),
+    'fhs': ('layer64_{}_HI8_AHI_TKY_FHS.shp', HI8_FHS_LEADTIME)
+}   
+# number of maps available in the rolling cache
+HI8_HISTORY = 134
+
+# formula to get the latest timestamp (rounded to 10 minutes)
+def himawari_latest_ts(leadtime):
+    now = datetime.now(tz=LANDGATE_TZ)-leadtime
+    result = now - timedelta(minutes=now.minute%10, seconds=now.second, microseconds=now.microsecond)
+    
+    return result   
+    
+@bottle.route('/himawari8/<target>')
+def himawari8(target):
+    layer_id, layer_lead = HI8[target]
+
+    now = himawari_latest_ts(leadtime=layer_lead)
+    query = {k.lower():v for k,v in bottle.request.query.items()}
+    
+    if 'layers' in query and query['layers'].isdigit():
+        layer = int(query['layers'])
+        ts = now - LANDGATE_PERIOD*layer
+        query['layers'] = layer_id.format(ts.strftime("%Y%m%d%H%M"))
+        
+    redirect = '{}?{}'.format(random.choice(LANDGATE_SERVERS), urllib.parse.urlencode(query))
+    
+    #return bottle.redirect('')
+    print('{}'.format((now, query, redirect)))
+    return bottle.redirect(redirect)
+
+
+# PDF renderer, accepts a JPG
 @bottle.route("/gdal/pdf", method="POST")
 def gdal_pdf():
     # needs gdal 1.10+
