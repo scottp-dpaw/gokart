062868a415b763cb89ad82876bb423c41da7dc91
Adon Metcalfe <adonm@fastmail.fm>
2016-06-16 16:52:52 +0800

    some print and ui fixes

--- gokart/static/js/ui.js
+++ gokart/static/js/ui.js
@@ -80,15 +80,17 @@ window.gokart = (function(self) {
     }
 
     // update "Map Layers" pane
-    ui.renderActiveLayers = function(ev) {
+    ui.renderActiveLayers = debounce(function(ev) {
         if (ui.updatingOrder) { return }
         ui.layersActive.html(ui.activeLayersTmpl({
             layers: self.map.getLayers().getArray()
         })).append(ui.layersActive.children().get().reverse());
-    }
+        ui.renderCatalogueLayers();
+    }, 250);
     
     // update "Layer Catalogue" pane
     ui.renderCatalogueLayers = function(ev) {
+        if (ui.updatingCatalogue) { return }
         $("#layers-catalogue-list").html(ui.catalogueLayersTmpl({
             layers: self.catalogue
         }));
@@ -105,6 +107,53 @@ window.gokart = (function(self) {
         ui.updatingOrder = false;
     }
 
+    ui.swapBaseLayers = true;
+
+    ui.initCatalogue = function(element) {
+        ui.catalogueLayersTmpl = Handlebars.compile($(element + "-template").html());
+        $(element).on("change", "div[data-id] input[data-action='toggle']", function() {
+            ui.updatingCatalogue = true;
+            var layer = self.catalogue[$(this).parents("div[data-id]").attr("data-id")];
+            // TODO: baselayer logic
+            if ($(this).prop('checked')) {
+                if (layer.base) {
+                    if (ui.swapBaseLayers) {
+                        self.map.getLayers().forEach(function(layer) {
+                            if (layer.get("catalogueEntry").base) {
+                                self.map.removeLayer(layer);
+                                layer.get("catalogueEntry").olLayer = undefined;
+                            }
+                        });
+                    }
+                    ui.updatingCatalogue = false;
+                    self.map.getLayers().insertAt(0, layer.init());
+                } else {
+                    self.map.addLayer(layer.init());
+                }
+            } else {
+                var olLayer = layer.olLayer;
+                layer.olLayer = undefined;
+                self.map.removeLayer(olLayer);
+            }
+            ui.updatingCatalogue = false;
+        });
+    }
+
+    ui.initActiveLayers = function(element) {
+        ui.activeLayersTmpl = Handlebars.compile($(element + "-template").html());
+        ui.layerDetailsTmpl = Handlebars.compile($("#layer-details-template").html());
+        ui.layersActive = $(element).on("click", "div[data-id]", function() {
+            $("#layer-details").html(ui.layerDetailsTmpl({
+                ol_layer: self.layerById($(this).attr("data-id")),
+                catalogue_layer: {abstract: "placeholder abstract"}
+            }));
+        }).on("click", "div[data-id] a[data-action='remove']", function() {
+            var layer = self.layerById($(this).parents("div[data-id]").attr("data-id"));
+            layer.get("catalogueEntry").olLayer = undefined;
+            self.map.removeLayer(layer);
+        })
+    }
+
     $self.on("init_map", function() {
         // setup scale events
         ui.menuScale = $("#menu-scale").on("change", function() { self.set_scale($(this).val().replace("1:", "").replace(/,/g, "").replace("K", "")) });
@@ -118,37 +167,11 @@ window.gokart = (function(self) {
         $("#download-pdf").on("click", function() { self.print("pdf") });
         // setup layer ordering if layer ui available
         if ($("#layers-active").length == 1) {
-            ui.activeLayersTmpl = Handlebars.compile($("#layers-active-template").html());
-            ui.layerDetailsTmpl = Handlebars.compile($("#layer-details-template").html());
-            ui.layersActive = $("#layers-active").on("click", "div[data-id]", function() {
-                $("#layer-details").html(ui.layerDetailsTmpl({
-                    ol_layer: self.layerById($(this).attr("data-id")),
-                    catalogue_layer: {abstract: "placeholder abstract"}
-                }));
-            }).on("click", "div[data-id] a[data-action='remove']", function() {
-                var layer = self.layerById($(this).parents("div[data-id]").attr("data-id"));
-                self.map.removeLayer(layer);
-                layer.catalogueEntry.olLayer = undefined;
-            })
-            ui.layersCatalogue = $("#layers-catalogue").on("change", "div[data-id] input[data-action='toggle']", function() {
-                var layer = self.catalogue[$(this).parents("div[data-id]").attr("data-id")];
-                // TODO: baselayer logic
-                if ($(this).prop('checked')) {
-                    console.log('adding');
-                    self.map.addLayer(layer.init());
-                } else {
-                    self.map.removeLayer(layer.olLayer);
-                    layer.olLayer = undefined;
-                }
-            });
+            ui.initActiveLayers("#layers-active");
+            ui.initCatalogue("#layers-catalogue");
+            ui.renderActiveLayers();
             self.map.getLayerGroup().on("change", ui.renderActiveLayers);
             dragula([ui.layersActive.get(0)]).on("dragend", ui.updateOrder);
-            ui.renderActiveLayers();
-        }
-
-        if ($("#layers-catalogue-list").length == 1) {
-            ui.catalogueLayersTmpl = Handlebars.compile($("#layers-catalogue-template").html());
-            ui.renderCatalogueLayers();
         }
     });
 
