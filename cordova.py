73b8bffd3527d386e459df0667a2fa2d4081a93f
Adon Metcalfe <adonm@fastmail.fm>
2016-07-13 23:35:53 +0800

    mostly working, auth needs some thought

--- cordova.py
+++ cordova.py
@@ -8,10 +8,14 @@ apps = ["sss"]
 
 for app in apps:
     os.chdir(os.path.join(basedir, "cordova/{}/www".format(app)))
-    subprocess.check_call(["wget", "--mirror", "--convert-links", "--span-hosts", "-P", "cache".format(app), "http://{}/{}".format(server, app)])
+    subprocess.check_call(["wget", "--mirror", "--content-disposition", "--span-hosts", 
+        "-P", "cache".format(app), "-r", "-p", "http://{}/{}".format(server, app)])
     subprocess.check_call(["rsync", "-Pavvr", os.path.join(basedir, "gokart/static/"), "cache/{}/static/".format(server)])
     subprocess.check_call(["rsync", "-av", "cache/{}/".format(server), "cache/base/"])
     subprocess.call(["mv", "cache/base/{}".format(app), "cache/base/index.html"])
+    # Make external asset links relative
+    subprocess.call(["bash", "-c", 'for file in $(find cache -name "*\?*"); do mv -v "$file" "${file%%\?*}"; done'])
+    subprocess.call(["sed", "-i", "bk", "s=//static.dpaw.wa.gov.au=../static.dpaw.wa.gov.au=g", "cache/base/index.html"])
     os.chdir("..")
     # Run launched command against each app
     subprocess.call(["cordova"] + sys.argv[1:])
