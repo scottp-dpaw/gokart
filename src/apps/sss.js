dc8240b0dbc1593256d6489cf9bd2e235104875e
Adon Metcalfe <adonm@fastmail.fm>
2016-07-15 00:42:42 +0800

    browserify shims

--- src/apps/sss.js
+++ src/apps/sss.js
@@ -1,16 +1,19 @@
-window.$ = window.jQuery = require('jquery');
+import $ from 'jquery'
+import '../../node_modules/foundation-sites/dist/foundation-flex.css'
 import 'foundation-sites'
 import svg4everybody from 'svg4everybody'
 import Vue from 'vue'
 import App from './sss.vue'
+
+global.$ = $
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: { App },
   onready: function () {
-    $(document).foundation();
-    svg4everybody();
+    $(document).foundation()
+    svg4everybody()
   }
 })
 
