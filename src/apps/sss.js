14acc3592714eab722b9eb2565ce4d0222e57010
Scott Percival <scott.percival@dpaw.wa.gov.au>
2016-07-15 10:31:05 +0800

    fix path typo in package.json

--- src/apps/sss.js
+++ src/apps/sss.js
@@ -1,18 +1,19 @@
-import 'foundation-sites/dist/foundation-flex.css'
-import 'font-awesome/css/font-awesome.css'
-import 'openlayers/dist/ol-debug.css'
-import 'dragula/dist/dragula.css'
+import 'foundation-sites/dist/foundation-flex.css';
+import 'font-awesome/css/font-awesome.css';
+import 'openlayers/dist/ol-debug.css';
+import 'dragula/dist/dragula.css';
 
-import $ from 'jquery'
-import 'foundation-sites'
-import svg4everybody from 'svg4everybody'
-import ol from 'openlayers/dist/ol-debug.js'
-import Vue from 'vue'
-import App from './sss.vue'
+import $ from 'jquery';
+import jQuery from 'jquery';
+import svg4everybody from 'svg4everybody';
+import 'foundation-sites';
+import ol from 'openlayers/dist/ol-debug.js';
+import Vue from 'vue';
+import App from './sss.vue';
 
 
-global.$ = $
-global.ol = ol
+global.$ = $;
+global.ol = ol;
 
 /* eslint-disable no-new */
 new Vue({
@@ -21,26 +22,26 @@ new Vue({
     App
   },
   ready: function () {
-    $(document).foundation()
-    svg4everybody()
+    $(document).foundation();
+    svg4everybody();
     // bind menu side-tabs to reveal the side pane
-    var offCanvasLeft = $('#offCanvasLeft')
+    var offCanvasLeft = $('#offCanvasLeft');
     $('#menu-tabs').on('change.zf.tabs', function (ev) {
-      offCanvasLeft.addClass('reveal-responsive')
-      // self.map.updateSize()
+      offCanvasLeft.addClass('reveal-responsive');
+      // self.map.updateSize();
     }).on('click', '.tabs-title a[aria-selected=false]', function (ev) {
-      offCanvasLeft.addClass('reveal-responsive')
-      $(this).attr('aria-selected', true)
-      // self.map.updateSize()
+      offCanvasLeft.addClass('reveal-responsive');
+      $(this).attr('aria-selected', true);
+      // self.map.updateSize();
     }).on('click', '.tabs-title a[aria-selected=true]', function (ev) {
-      offCanvasLeft.removeClass('reveal-responsive')
-      $(this).attr('aria-selected', false)
-      // self.map.updateSize()
-    })
+      offCanvasLeft.removeClass('reveal-responsive');
+      $(this).attr('aria-selected', false);
+      // self.map.updateSize();
+    });
     $('#side-pane-close').on('click', function (ev) {
-      offCanvasLeft.removeClass('reveal-responsive')
-      $('#menu-tabs').find('.tabs-title a[aria-selected=true]').attr('aria-selected', false)
-      // self.map.updateSize()
-    })
+      offCanvasLeft.removeClass('reveal-responsive');
+      $('#menu-tabs').find('.tabs-title a[aria-selected=true]').attr('aria-selected', false);
+      // self.map.updateSize();
+    });
   }
-})
+});
