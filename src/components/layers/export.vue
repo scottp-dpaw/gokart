740062ad1a0ff4f77ae523cf20b12a1ebaf79a71
Adon Metcalfe <adonm@fastmail.fm>
2016-07-19 22:28:24 +0800

    fixed print regression

--- src/components/layers/export.vue
+++ src/components/layers/export.vue
@@ -51,7 +51,7 @@
   import { kjua, saveAs, moment, $, localforage } from 'src/vendor.js'
   import gkLegend from './legend.vue'
   export default {
-    store: ['whoami', 'dpmm', 'view'],
+    store: ['whoami', 'dpmm', 'view', 'mmPerInch'],
     components: { gkLegend },
     data: function () {
       return {
@@ -110,7 +110,7 @@
       setSize: function () {
         $('body').css('cursor', 'progress')
         this.layout = this.mapLayout
-        this.dpmm = this.minDPI / this.$root.mmPerInch
+        this.dpmm = this.minDPI / this.mmPerInch
         this.olmap.setSize([this.dpmm * this.layout.width, this.dpmm * this.layout.height])
         this.olmap.getView().fit(this.layout.extent, this.olmap.getSize())
         this.$root.map.setScale(this.$root.map.getFixedScale())
