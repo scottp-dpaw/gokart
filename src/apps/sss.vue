ca1bef085b149ccea03426183ec4e674c6120348
Adon Metcalfe <adonm@fastmail.fm>
2016-07-27 17:59:53 +0800

    generic svg renderer

--- src/apps/sss.vue
+++ src/apps/sss.vue
@@ -37,6 +37,11 @@
             </div>
         </div>
     </div>
+    <div class="hide">
+      <div v-for="blob in svgBlobs">
+        <img v-bind:src="blob"/>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -44,8 +49,77 @@
     import gkLayers from '../components/layers.vue'
     import gkAnnotations from '../components/annotations.vue'
     import gkTracking from '../components/sss/tracking.vue'
+    import { ol } from 'src/vendor.js'
 
-    export default { components: { gkMap, gkLayers, gkAnnotations, gkTracking } }
+
+    export default { 
+      components: { gkMap, gkLayers, gkAnnotations, gkTracking },
+      data: function() {
+        var stroke = '#ff6600'
+        var fill = '#7c3100'
+        return {
+          tints: {
+            'red': [[stroke,'#ed2727'], [fill,'#480000']],
+            'orange': [[stroke,'#ff6600'], [fill,'#562200']],
+            'yellow': [[stroke,'#ffd700'], [fill,'#413104']],
+            'green': [[stroke,'#71c837'], [fill,'#1b310d']],
+            'selected': [[stroke,'#ffffff'], [fill,'#2199e8']]
+          },
+          svgTemplates: {},
+          svgBlobs: {}
+        }
+      },
+      methods: {
+        tintSVG: function(svgstring, tintKey) {
+          this.tints[tintKey].forEach(function(colour) {
+            svgstring = svgstring.split(colour[0]).join(colour[1])
+          })
+          return svgstring
+        },
+        getBlob: function(url, tintKey) {
+          var key = url + '#' + tintKey
+          if (this.svgBlobs[key]) {
+            return this.svgBlobs[key]
+          } else {
+            this.addSVG(url)
+          }
+        },
+        addSVG: function(url, tints, dims) {
+          var vm = this
+          if (!tints) {
+            // tint everything if not specified
+            var tints = Object.keys(this.tints)
+          }
+          if (!dims) {
+            dims = [48, 48]
+          }
+          if (this.svgTemplates[url]) {
+            return
+          }
+          var svg = this.svgTemplates[url] = { url: url, tints: tints, dims: dims }
+          var req = new window.XMLHttpRequest()
+          req.withCredentials = true
+          req.onload = function () {
+            var svgstring = this.responseText
+            svg.tints.forEach(function(tint) {
+              var canvas = $('<canvas>')
+              canvas.attr({width: svg.dims[0], height: svg.dims[1]})
+              canvas.drawImage({
+                source: 'data:image/svg+xml;utf8,' + encodeURIComponent(vm.tintSVG(svgstring, tint)),
+                fromCenter: false, x: 0, y: 0, width: dims[0], height: dims[1],
+                load: function () {
+                  canvas.get(0).toBlob(function (blob) {
+                    vm.svgBlobs[svg.url + '#' + tint] = window.URL.createObjectURL(blob)
+                  }, 'image/png')
+                }
+              })
+            })
+          }
+          req.open('GET', svg.url)
+          req.send()
+        }
+      }
+    }
 </script>
 
 <style>
